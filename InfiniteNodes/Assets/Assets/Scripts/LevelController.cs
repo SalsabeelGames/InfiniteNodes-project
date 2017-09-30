80f330dec047f53658fd554f72ebcff57e53aee7
mhnd-abu-safia <mhnd.abu.safia@hotmail.com>
2017-09-30 16:12:45 +0300

    First Commit
    Add InfiniteNodes projects to githup

---
+++ InfiniteNodes/Assets/Assets/Scripts/LevelController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour {
+
+	public LevelButton buton;
+	public GameObject cv;
+	void Start () {
+		LevelButton b = Instantiate (buton);
+		b.transform.position = new Vector3 ();
+		b.transform.SetParent (cv.transform, false);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void clickLevel(int levelNumber){
+
+	}
+}
